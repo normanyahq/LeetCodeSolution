1486676cc63202d13326d959a91c61058c73ba23
牙河清 <Norman@yaheqingdeMBP.wv.cc.cmu.edu>
2015-11-23 21:06:24 -0500

    Min Stack.

---
+++ Min Stack/solution.py
@@ -0,0 +1,36 @@
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        t = self.stack.pop()
+        if t == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
